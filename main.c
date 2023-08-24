9127c747f8b456a2d207f80548b46f64ccc753ac
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2023-08-24 20:13:47 -0300

    autosave

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <time.h>
-#include <json-c/json.h>
+#include <stdlib.h> // para exit()
 #include "default.h"
 #include "controlles.h"
 #include "getdatelocal.h"
 
-//gcc -o main main.c -ljson-c
 
+//gcc -o main main.c -ljson-c
 
 int main(){
 	int opcaomenu0;
-	char nametask[50];
 	struct dateatul;
 	struct Date date;
-	struct json_object *json_obj = json_object_new_object();
 	char date_str[11];
-	// json_object_object_add(json_obj, "age", json_object_new_int(30));
-
+	char nametask[50];
+	char namefile_litstasks[] = "tasks";
+	char stringdata_task[255];
+	int i;
 	while (1){
 		opcaomenu0 = menu0return();
 		// printf("\n%d escolhido.\n",opcaomenu0);
-		// ("teste");
-
 		switch (opcaomenu0){
 			case 1:
 				printf("digite o nome da tarefa\n>> ");
@@ -29,14 +27,20 @@ int main(){
 				printf("criando tarefa %s\n",&nametask);
                 date = getdatelocal();
                 sprintf(date_str,"%d/%d/%d\n",date.day,date.month,date.year);
-
-                json_object_object_add(json_obj, "nametask", json_object_new_string(nametask));
-                json_object_object_add(json_obj,"dateinit",json_object_new_string(date_str));
-                const char *json_str = json_object_to_json_string(json_obj);
-				printf("JSON: %s\n", json_str);
-
-                const char *strjson = json_object_get_string(json_obj);
+                // printf("%s",date_str);
+               	sprintf(stringdata_task,"|nametask %s |datenewtask %s",&nametask, &date_str);
                
+                FILE *fp;
+                fp = fopen(namefile_litstasks, "a+");
+                if (!fp){
+                	printf(stringdata_task,"Error na leitura de file tasks");
+                	// continue;
+                	exit(1);
+                	}
+                	else{
+                		for(i=0;stringdata_task[i];i++) putc(stringdata_task[i],fp);
+                	}
+                fclose(fp);
                 // sscanf("%d/%d/%d\n",date.day,date.month,date.year);
               
 			case 2:
